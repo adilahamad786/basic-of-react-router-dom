3b7f5b11d1ceb70f2bb8dbe1499a4c68e829f6be
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-19 16:11:56 +0530

    Bad way to navigate pages by clicking. (becuase lost state data and reload or refetch page data from server).

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import Product from "./components/Product";
-import Welcome from "./components/Welcome";
+import MainHeader from "./components/MainHeader";
+import Product from "./pages/Product";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<h1>The home page.</h1>} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/product" element={<Product />} />
-      </Routes>
+      <main>
+        <MainHeader />
+        <Routes>
+          <Route path="/" element={<h1>The home page.</h1>} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/components/MainHeader.js
@@ -0,0 +1,18 @@
+const MainHeader = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/welcome">Welcome</a>
+          </li>
+          <li>
+            <a href="/product">Product</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
